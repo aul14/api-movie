61ae5b8406c380ae73ebb55caed1d2215ff7ace8
aul14 <rahmanaulia336@gmail.com>
2024-01-08 16:13:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# API Movie With Lumen PHP Framework
+
+# Cara Instalasi, Menjalankan, dan Pengujian Aplikasi.
+
+1. Clone atau Download dan ekstrak, aplikasi api-movie.
+2. Buat database MySQL dengan nama "api_movie" (tanpa tanda kutip).
+3. Buka command prompt atau git bash dan arahkan ke aplikasi api-movie.
+4. Jalankan perintah "php artisan migrate" (tanpa tanda kutip), perintah tersebut untuk melakukan migrasi database aplikasi api-movie.
+5. Jalankan perintah "php -S localhost:8000 -t public" (tanpa tanda kutip), perintah tersebut untuk menjalankan aplikasi api-movie.
+
+# Untuk melakukan testing apakah aplikasi berjalan dengan baik atau tidak, bisa langsung melalui aplikasi postman. Untuk caranya sebagai berikut:
+
+1. Buka aplikasi postman.
+2. Import collection file "API-MOVIES.postman_collection.json", yang sudah disediakan di dalam folder aplikasi api-movie, selanjutnya di dalam folder "Collection Api Movie".
+3. Lalu jalankan collection api tersebut.
+
+Happy Coding =).
---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class MovieController extends Controller
+{
+    public function index() 
+    {
+        $data = Movie::all();
+        return response()->json([
+            'message'   => 'success',
+            'data'      => $data
+        ]);
+    }
+    
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title'    => 'required|max:150|unique:movies,title',
+            'rating'    => 'required|numeric',
+            'description' => 'required',
+            'image' => 'nullable'
+        ]);
+
+        DB::beginTransaction();
+        try {
+            $mv = new Movie();
+            $mv->title = $request->title;
+            $mv->description = $request->description;
+            $mv->rating = $request->rating;
+            $mv->image = $request->image;
+            $mv->save();
+
+            DB::commit();
+            return response()->json([
+                'message'   => 'success',
+                'data'      => $mv
+            ], 201);
+        } catch (\Throwable $th) {
+            DB::rollback();
+            abort('402', $th->getMessage());
+        }
+    }
+
+    public function show($id) 
+    {
+        $data = Movie::find($id);
+        if (empty($data)) {
+            abort('404', 'Data not found!');
+        }
+
+        return response()->json([
+            'message'   => 'success',
+            'data'      => $data
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title'    => 'required|max:150|unique:movies,title,' . $id,
+            'rating'    => 'required|numeric',
+            'description' => 'required',
+            'image' => 'nullable'
+        ]);
+
+        DB::beginTransaction();
+        try {
+            $mv = Movie::find($id);
+            if (empty($mv)) {
+                abort('404', 'Data not found!');
+            }
+
+            $mv->title = $request->title;
+            $mv->description = $request->description;
+            $mv->rating = $request->rating;
+            $mv->image = $request->image;
+            $mv->update();
+
+            DB::commit();
+            return response()->json([
+                'message'   => 'success',
+                'data'      => $mv
+            ]);
+        } catch (\Throwable $th) {
+            DB::rollback();
+            abort('402', $th->getMessage());
+        }
+    }
+
+    public function destroy($id) 
+    {
+        $data = Movie::find($id);
+        if (empty($data)) {
+            abort('404', 'Data not found!');
+        }
+        $data->delete();
+        return response()->json([
+            'message'   => 'success',
+        ]);
+    }
+}
